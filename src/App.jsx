7c46036479ba36c303dc05cdd2c70d2508e6fa34
Simone Cimminelli <simonecimminelli@simones-air.home>
2024-08-04 21:26:49 +0100

    Remove App Header from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 import AirtableEveningRoutineChecker from './components/AirtableEveningRoutineChecker'
 
 function App() {
-  console.log("App component rendering");
-  try {
-    return (
-      <div className="App">
-        <h1>App Header</h1>
-        <AirtableEveningRoutineChecker />
-      </div>
-    )
-  } catch (error) {
-    console.error("Error rendering App:", error);
-    return <div>An error occurred</div>;
-  }
+  return (
+    <div className="App">
+      <AirtableEveningRoutineChecker />
+    </div>
+  )
 }
 
 export default App
